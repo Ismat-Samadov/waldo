df306e60aec91b79d228a37238471c93d5150196
Ismat-Samadov <ismetsemedov@gmail.com>
2024-03-22 02:35:54 +0400

    check

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,8 @@ const user_controller = require("../controllers/userController");
 router.get("/coordinates", coordinate_controller.coordinate_list_get);
 router.get("/coordinates/:name&:x&:y", coordinate_controller.coordinate_get);
 
-router.get("/user", user_controller.user_list_get);
-router.post("/user", user_controller.user_post);
-router.post("/user/:id", user_controller.user_put);
+router.get("/users", user_controller.user_list_get);
+router.post("/users", user_controller.user_post);
+router.post("/users/:id", user_controller.user_put);
 
 module.exports = router;    
