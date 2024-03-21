c4946957fc8057fb398301c7e0362894945bffb3
Ismat-Samadov <ismetsemedov@gmail.com>
2024-03-21 23:58:34 +0400

    style

--- models/coordinate.js
+++ models/coordinate.js
@@ -7,3 +7,26 @@ const coordinateSchema = new Schema({
 	y: { type: Number, required: true },
 });
 module.exports = mongoose.model("coordinate", coordinateSchema);
+
+
+
+// curl - X POST \
+// https://waldo-4knp.onrender.com/api/coordinates/bulk \
+// -H 'Content-Type: application/json' \
+// -d '[
+// {
+// 	"name": "odlaw",
+// 		"x": 11,
+// 			"y": 37
+// },
+// {
+// 	"name": "wizard",
+// 		"x": 27,
+// 			"y": 37
+// },
+// {
+// 	"name": "waldo",
+// 		"x": 62,
+// 			"y": 37
+// }
+// ]'
