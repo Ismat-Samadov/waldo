50c6cfcd6695f145cb70fd31457c7294c3e80ca7
Ismat-Samadov <ismetsemedov@gmail.com>
2024-03-22 00:24:55 +0400

    check

--- controllers/coordinateController.js
+++ controllers/coordinateController.js
@@ -1,15 +1,10 @@
-// controllers/coordinateController.js
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 const Coordinate = require("../models/coordinate");
-
-// Retrieve all coordinates
 exports.coordinate_list_get = asyncHandler(async (req, res, next) => {
-	const allCoordinates = await Coordinate.find().exec();
-	return res.json(Object.values(allCoordinates));
+	const allCoordinate = await Coordinate.find().exec();
+	return res.json(Object.values(allCoordinate));
 });
-
-// Retrieve a specific coordinate
 exports.coordinate_get = asyncHandler(async (req, res, next) => {
 	const coordinateCharacter = await Coordinate.findOne({
 		name: req.params.name,
@@ -25,22 +20,4 @@ exports.coordinate_get = asyncHandler(async (req, res, next) => {
 		isCoordinateClose = true;
 
 	return res.send(isCoordinateClose);
-});
-
-// Create a new coordinate
-exports.coordinate_create_post = asyncHandler(async (req, res, next) => {
-	// Extract data from request body
-	const { name, x, y } = req.body;
-
-	// Create a new coordinate instance
-	const newCoordinate = new Coordinate({ name, x, y });
-
-	// Save the new coordinate to the database
-	try {
-		const savedCoordinate = await newCoordinate.save();
-		res.status(201).json(savedCoordinate);
-	} catch (error) {
-		console.error("Error creating coordinate:", error);
-		res.status(500).json({ message: "Internal server error" });
-	}
 });
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,14 @@
 // routes/index.js
-const express = require("express");
-const router = express.Router();
+var express = require("express");
+var router = express.Router();
 const coordinate_controller = require("../controllers/coordinateController");
 const user_controller = require("../controllers/userController");
 
-// Existing routes
 router.get("/coordinates", coordinate_controller.coordinate_list_get);
 router.get("/coordinates/:name&:x&:y", coordinate_controller.coordinate_get);
+
 router.get("/users", user_controller.user_list_get);
 router.post("/users", user_controller.user_post);
 router.post("/users/:id", user_controller.user_put);
-
-// New route for creating coordinates
-router.post("/coordinates", coordinate_controller.coordinate_create_post);
 
 module.exports = router;
